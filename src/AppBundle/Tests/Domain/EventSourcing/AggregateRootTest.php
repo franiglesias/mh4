a640669ce58e021f0ccd5c34bf0a93ead16e8f44
franiglesias <franiglesias@mac.com>
2015-12-03 17:02:24 +0100

    Added some more interest

--- src/AppBundle/Tests/Domain/EventSourcing/AggregateRootTest.php
+++ src/AppBundle/Tests/Domain/EventSourcing/AggregateRootTest.php
@@ -8,48 +8,58 @@ use AppBundle\Domain\EventSourcing\DomainEvent;
 class Aggregate extends AggregateRoot {
 	private $id;
 	private $state;
+	private $lastMessage;
 	
 	public function __construct()
 	{
 		$this->state = 0;
+		$this->lastMessage = '';
 	}
 	
 	static function reconstituteFrom($events)
 	{
 		$aggregate = new self();
 		foreach ($events as $event) {
-			$aggregate->recordThat($event);
+			$aggregate->apply($event);
 		}
 		return $aggregate;
 	}
 	
-	public function doSomething()
+	public function doSomething($message)
 	{
-		$this->recordThat(new EventHappened($this->id));
+		$this->recordThat(new EventHappened($this->id, $message));
 	}
 	
 	protected function applyEventHappened(EventHappened $event)
 	{
+		$this->lastMessage = $event->getArgument();
 		++$this->state;
 	}
 	
 	public function getState()
 	{
-		return $this->state;
+		return $this->lastMessage;
 	}
 }
 
 class EventHappened implements DomainEvent {
 	
 	private $aggregate_id;
+	private $argument;
 	
-	public function __construct($aggregate_id)
+	public function __construct($aggregate_id, $argument)
 	{
 		$this->aggregate_id = $aggregate_id;
+		$this->argument = $argument;
 	}
 	public function getAggregateId()
 	{
 		return $this->aggregate_id;
+	}
+	
+	public function getArgument()
+	{
+		return $this->argument;
 	}
 }
 
@@ -64,26 +74,24 @@ class AggregateRootTests extends \PHPUnit_Framework_Testcase {
 	public function testAggregateRecordsEvents()
 	{
 		$A = new Aggregate();
-		$A->doSomething();
+		$A->doSomething('Hello');
 		$this->assertEquals(1, count($A->getRecordedEvents()));
-		
-		$A->doSomething();
+		$this->assertEquals('Hello', $A->getState());
+		$A->doSomething('Hello again');
 		$this->assertEquals(2, count($A->getRecordedEvents()));
+		$this->assertEquals('Hello again', $A->getState());
 		
-		$this->assertEquals(2, $A->getState());
 	}
 	
-	public function testWeCanReconstuteAnAggregateFromEventsStory()
+	public function testWeCanReconstituteAnAggregateFromEventsStory()
 	{
 		$A = new Aggregate();
-		$A->doSomething();
-		$A->doSomething();
+		$A->doSomething('Hello');
+		$A->doSomething('Hello again');
 		
 		$B = Aggregate::reconstituteFrom($A->getRecordedEvents());
-		$this->assertEquals(2, $B->getState());
-		$this->assertEquals(2, count($B->getRecordedEvents()));
-		
-		$this->assertEquals($B, $A);
+		$this->assertEquals(0, count($B->getRecordedEvents()));
+		$this->assertEquals('Hello again', $B->getState());
 	}
 	
 }
